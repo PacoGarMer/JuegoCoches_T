5f7e24b90ebffbc3c3a9d9253c9c6a2f626e3923
PacoGarMer <153186924+PacoGarMer@users.noreply.github.com>
2025-01-13 19:48:31 +0100

    Clase13_1

---
+++ Coches_T/Assets/Scripts/RetrovisorControl.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class RetrovisorControl : MonoBehaviour
+{
+    [SerializeField] GameObject retrovisor;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)){
+            retrovisor.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.R)){
+            retrovisor.SetActive(false);
+        }
+    }
+}
